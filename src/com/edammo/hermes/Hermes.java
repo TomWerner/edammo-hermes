89d5061aa5128ce8edf811ad8a96947df6667852
andrew-wm-arthur <andy.wm.arthur@gmail.com>
2016-10-31 15:34:55 -0500

    first run of hermes

--- src/com/edammo/hermes/Hermes.java
+++ src/com/edammo/hermes/Hermes.java
@@ -1,174 +1,162 @@
 package com.edammo.hermes;
-/* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved.  This code is subject to the terms
- * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
+
 /**
- * Extended by Andy on 10/25/2016.
+ * Created by Andy on 10/25/2016.
  */
 
-import java.io.IOException;
-import java.util.ArrayList;
+import com.google.api.client.http.*;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.json.JsonObjectParser;
+import com.google.api.client.util.Key;
 
-import com.ib.controller.ApiConnection;
-import com.ib.controller.ApiController;
-import com.ib.controller.Formats;
+import com.ib.controller.NewContract;
+import com.ib.controller.NewOrder;
+import com.ib.controller.OrderType;
 import com.ib.controller.Types;
 
-public class Hermes implements ApiController.IConnectionHandler {
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
-    private final AndyLogger m_inLogger = new AndyLogger();
-    private final AndyLogger m_outLogger = new AndyLogger();
-    private final ApiController m_controller = new ApiController( this, m_inLogger, m_outLogger);
-    private final ArrayList<String> m_acctList = new ArrayList<>();
-    private final PredictionHubConnection pred_hub_conn;
 
-    // getter methods
-    public ArrayList<String> accountList() 	{ return m_acctList; }
-    public ApiController controller() 		{ return m_controller; }
+public class Hermes {
+    private static final String GET_CONFIG_URL_SUFFIX = "/api/hermes/configuration";
+    private static final String UPDATE_PRICES_URL_SUFFIX = "/predictions/update_prices/";
+    private static final String GET_ORDERS_URL_SUFFIX = "/api/hermes/order";
 
-    private Hermes() throws IOException {
-        pred_hub_conn = new PredictionHubConnection("127.0.0.1", "8000");
-    }
+    private final GenericUrl config_url;
+    private final GenericUrl prices_url;
+    private final GenericUrl orders_url;
+    private final HttpRequestFactory requestFactory;
+    private Configuration config;
 
-    public static void main(String[] args) throws InterruptedException{
-        try{
-            new Hermes().middleman_loop();
-        } catch (IOException e){
-            System.exit(-1);
+
+    public static class TestResponse {
+        // Extra keys are ignored, missing keys are filled with null
+        @Key
+        public String test;
+
+        @Key
+        public String absent;
+
+        @Override
+        public String toString() {
+            return "{ test: " + test + ", absent: " + absent + " }";
         }
     }
 
-    private void middleman_loop() throws InterruptedException, IOException {
-        m_controller.connect( "127.0.0.1", 7497, 0);
-        pred_hub_conn.connect();
-        pred_hub_conn.get_configuration();
-        while(true){
-            pred_hub_conn.upload_prices();
-            pred_hub_conn.get_orders();
-            pred_hub_conn.upload_account_status();
-            pred_hub_conn.get_configuration();
-            Thread.sleep(1000);
+    public static class Configuration {
+        @Key
+        public String account;
+
+        @Key
+        public String currency;
+
+        @Key
+        public List<String> stock_symbols;
+
+        @Key
+        public List<String> stock_exchanges;
+    }
+
+    public static class ActionList {
+        @Key
+        public List<Action> actions;
+    }
+
+    public static class Action {
+        @Key
+        public String symbol;
+
+        @Key
+        public int shares;
+
+        @Key
+        public String exchange;
+
+        @Key
+        public String primaryExchange;
+
+        @Key
+        public String orderType;
+    }
+
+
+    private static class HermesContract extends NewContract {
+        HermesContract(Configuration c, int index) {
+            this.symbol(c.stock_symbols.get(index));
+            this.secType(Types.SecType.STK);
+            this.exchange("SMART");
+            this.primaryExch(c.stock_exchanges.get(index));
+            this.currency(c.currency);
+        }
+        HermesContract(Action a, Configuration c) {
+            this.symbol(a.symbol);
+            this.secType(Types.SecType.STK);
+            this.exchange(a.exchange);
+            this.primaryExch(a.primaryExchange);
+            this.currency(c.currency);
         }
     }
 
-    private void get_prices(){}
-    private void place_orders(){}
-    private void get_account_status(){}
+    private static class HermesOrder extends NewOrder {
+        HermesOrder(Action a, Configuration c) {
+            this.account(c.account);
+            this.action((a.orderType.equals("buy") ? Types.Action.BUY : Types.Action.SELL));
+            this.totalQuantity(a.shares);
+            this.orderType(OrderType.MKT);
+        }
+    }
 
+    public static class HermesContractOrder {
+        final NewContract c;
+        final NewOrder o;
+        HermesContractOrder(Action a, Configuration c) {
+            this.c = new HermesContract(a, c);
+            this.o = new HermesOrder(a, c);
+        }
+    }
 
-    // ---------------------------------------- IConnectionHandler ----------------------------------------
-    @Override public void connected() {
-        show( "connected");
+    Hermes(String prediction_hub_ip, String prediction_hub_port) {
+        config_url = new GenericUrl("http://" + prediction_hub_ip + ":" + prediction_hub_port + GET_CONFIG_URL_SUFFIX);
+        prices_url = new GenericUrl("http://" + prediction_hub_ip + ":" + prediction_hub_port + UPDATE_PRICES_URL_SUFFIX);
+        orders_url = new GenericUrl("http://" + prediction_hub_ip + ":" + prediction_hub_port + GET_ORDERS_URL_SUFFIX);
 
-        m_controller.reqCurrentTime( new ApiController.ITimeHandler() {
-            @Override public void currentTime(long time) {
-                show( "Server date/time is " + Formats.fmtDate(time * 1000) );
+        requestFactory = new NetHttpTransport().createRequestFactory(new HttpRequestInitializer() {
+            @Override
+            public void initialize(HttpRequest request) {
+                request.setParser(new JsonObjectParser(new GsonFactory()));
             }
         });
-
-        m_controller.reqBulletins( true, new ApiController.IBulletinHandler() {
-            @Override public void bulletin(int msgId, Types.NewsType newsType, String message, String exchange) {
-                String str = String.format( "Received bulletin:  type=%s  exchange=%s", newsType, exchange);
-                show( str);
-                show( message);
-            }
-        });
     }
 
-    @Override public void disconnected() {
-        show( "disconnected");
+    void get_configuration() throws IOException {
+        config = requestFactory.buildGetRequest(config_url).execute().parseAs(Configuration.class);
     }
 
-    @Override public void accountList(ArrayList<String> list) {
-        show( "Received account list");
-        m_acctList.clear();
-        m_acctList.addAll( list);
-    }
-
-    @Override public void show( final String str) {
-        m_outLogger.log(str);
-    }
-
-    @Override public void error(Exception e) {
-        show( e.toString() );
-    }
-
-    @Override public void message(int id, int errorCode, String errorMsg) {
-        show( id + " " + errorCode + " " + errorMsg);
-    }
-
-    private static class AndyLogger implements ApiConnection.ILogger {
-        AndyLogger() {}
-
-        @Override public void log(final String str) {
-            System.out.print(str);
+    ArrayList<HermesContractOrder> get_orders() throws IOException {
+        ActionList action_list = requestFactory.buildGetRequest(orders_url).execute().parseAs(ActionList.class);
+        ArrayList<HermesContractOrder> order_list = new ArrayList<>();
+        for(Action a: action_list.actions) {
+            order_list.add(new HermesContractOrder(a, config));
         }
+        return order_list;
     }
+
+    List<HermesContract> get_current_stocks() {
+        ArrayList<HermesContract> cur_stocks = new ArrayList<>();
+        for(int i = 1; i < config.stock_symbols.size(); i++) {
+            cur_stocks.add(new HermesContract(config, i));
+        }
+        return cur_stocks;
+    }
+
+    void upload_prices() {
+    }
+
+    void upload_account_status() {
+    }
+
+
 }
-
-// do clearing support
-// change from "New" to something else
-// more dn work, e.g. deltaNeutralValidation
-// add a "newAPI" signature
-// probably should not send F..A position updates to listeners, at least not to API; also probably not send FX positions; or maybe we should support that?; filter out models or include it
-// finish or remove strat panel
-// check all ps
-// must allow normal summary and ledger at the same time
-// you could create an enum for normal account events and pass segment as a separate field
-// check pacing violation
-// newticktype should break into price, size, and string?
-// give "already subscribed" message if appropriate
-
-// BUGS
-// When API sends multiple snapshot requests, TWS sends error "Snapshot exceeds 100/sec" even when it doesn't
-// When API requests SSF contracts, TWS sends both dividend protected and non-dividend protected contracts. They are indistinguishable except for having different conids.
-// Fundamentals financial summary works from TWS but not from API
-// When requesting fundamental data for IBM, the data is returned but also an error
-// The "Request option computation" method seems to have no effect; no data is ever returned
-// When an order is submitted with the "End time" field, it seems to be ignored; it is not submitted but also no error is returned to API.
-// Most error messages from TWS contain the class name where the error occurred which gets garbled to gibberish during obfuscation; the class names should be removed from the error message
-// If you exercise option from API after 4:30, TWS pops up a message; TWS should never popup a message due to an API request
-// TWS did not desubscribe option vol computation after API disconnect
-// Several error message are misleading or completely wrong, such as when upperRange field is < lowerRange
-// Submit a child stop with no stop price; you get no error, no rejection
-// When a child order is transmitted with a different contract than the parent but no hedge params it sort of works but not really, e.g. contract does not display at TWS, but order is working
-// Switch between frozen and real-time quotes is broken; e.g.: request frozen quotes, then realtime, then request option market data; you don't get bid/ask; request frozen, then an option; you don't get anything
-// TWS pops up mkt data warning message in response to api order
-
-// API/TWS Changes
-// we should add underConid for sec def request sent API to TWS so option chains can be requested properly
-// reqContractDetails needs primary exchange, currently only takes currency which is wrong; all requests taking Contract should be updated
-// reqMktDepth and reqContractDetails does not take primary exchange but it needs to; ideally we could also pass underConid in request
-// scanner results should return primary exchange
-// the check margin does not give nearly as much info as in TWS
-// need clear way to distinguish between order reject and warning
-
-// API Improvements
-// add logging support
-// we need to add dividendProt field to contract description
-// smart live orders should be getting primary exchange sent down
-
-// TWS changes
-// TWS sends acct update time after every value; not necessary
-// support stop price for trailing stop order (currently only for trailing stop-limit)
-// let TWS come with 127.0.0.1 enabled, same is IBG
-// we should default to auto-updating client zero with new trades and open orders
-
-// NOTES TO USERS
-// you can get all orders and trades by setting "master id" in the TWS API config
-// reqManagedAccts() is not needed because managed accounts are sent down on login
-// TickType.LAST_TIME comes for all top mkt data requests
-// all option ticker requests trigger option model calc and response
-// DEV: All Box layouts have max size same as pref size; but center border layout ignores this
-// DEV: Box layout grows items proportionally to the difference between max and pref sizes, and never more than max size
-
-//TWS sends error "Snapshot exceeds 100/sec" even when it doesn't; maybe try flush? or maybe send 100 then pause 1 second? this will take forever; i think the limit needs to be increased
-
-//req open orders can only be done by client 0 it seems; give a message
-//somehow group is defaulting to EqualQuantity if not set; seems wrong
-//i frequently see order canceled - reason: with no text
-//Missing or invalid NonGuaranteed value. error should be split into two messages
-//Rejected API order is downloaded as Inactive open order; rejected orders should never be sen
-//Submitting an initial stop price for trailing stop orders is supported only for trailing stop-limit orders; should be supported for plain trailing stop orders as well
-//EMsgReqOptCalcPrice probably doesn't work since mkt data code was re-enabled
-//barrier price for trail stop lmt orders why not for trail stop too?
-//All API orders default to "All" for F; that's not good
---
+++ src/com/edammo/hermes/TWSConnection.java
@@ -0,0 +1,190 @@
+package com.edammo.hermes;
+/* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved.  This code is subject to the terms
+ * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
+/**
+ * Extended by Andy on 10/25/2016.
+ */
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ib.controller.*;
+
+import com.edammo.hermes.Hermes.*;
+
+public class TWSConnection implements ApiController.IConnectionHandler {
+
+    private final AndyLogger m_inLogger = new AndyLogger();
+    private final AndyLogger m_outLogger = new AndyLogger();
+    private final ApiController m_controller = new ApiController( this, m_inLogger, m_outLogger);
+    private final ArrayList<String> m_acctList = new ArrayList<>();
+    private final Hermes hermes;
+
+    // getter methods
+    public ArrayList<String> accountList() 	{ return m_acctList; }
+    public ApiController controller() 		{ return m_controller; }
+
+    private TWSConnection() throws IOException {
+        m_controller.connect( "127.0.0.1", 7497, 0);
+        hermes = new Hermes("127.0.0.1", "8000");
+    }
+
+    public static void main(String[] args) throws InterruptedException{
+        try{
+            new TWSConnection().middleman_loop();
+        } catch (IOException e){
+            System.exit(-1);
+        }
+    }
+
+    private void middleman_loop() throws InterruptedException, IOException {
+        hermes.get_configuration();
+        while(true){
+            hermes.upload_prices();
+            place_orders(hermes.get_orders());
+            hermes.upload_account_status();
+            hermes.get_configuration();
+            Thread.sleep(1000);
+        }
+    }
+
+    private void place_orders(ArrayList<HermesContractOrder> contract_orders_list){
+        for(HermesContractOrder co: contract_orders_list) {
+            m_controller.placeOrModifyOrder(co.c, co.o, new ApiController.IOrderHandler() {
+                @Override
+                public void orderState(NewOrderState orderState) {
+                }
+
+                @Override
+                public void orderStatus(OrderStatus status, int filled, int remaining, double avgFillPrice, long permId,
+                                        int parentId, double lastFillPrice, int clientId, String whyHeld) {
+                }
+
+                @Override
+                public void handle(int errorCode, final String errorMsg) {
+                }
+            });
+        }
+    }
+
+    private void get_account_status(){}
+
+
+    // ---------------------------------------- IConnectionHandler ----------------------------------------
+    @Override public void connected() {
+        show( "connected");
+
+        m_controller.reqCurrentTime( new ApiController.ITimeHandler() {
+            @Override public void currentTime(long time) {
+                show( "Server date/time is " + Formats.fmtDate(time * 1000) );
+            }
+        });
+
+        m_controller.reqBulletins( true, new ApiController.IBulletinHandler() {
+            @Override public void bulletin(int msgId, Types.NewsType newsType, String message, String exchange) {
+                String str = String.format( "Received bulletin:  type=%s  exchange=%s", newsType, exchange);
+                show( str);
+                show( message);
+            }
+        });
+    }
+
+    @Override public void disconnected() {
+        show( "disconnected");
+    }
+
+    @Override public void accountList(ArrayList<String> list) {
+        show( "Received account list");
+        m_acctList.clear();
+        m_acctList.addAll( list);
+    }
+
+    @Override public void show( final String str) {
+        m_outLogger.log(str);
+    }
+
+    @Override public void error(Exception e) {
+        show( e.toString() );
+    }
+
+    @Override public void message(int id, int errorCode, String errorMsg) {
+        show( id + " " + errorCode + " " + errorMsg);
+    }
+
+    private static class AndyLogger implements ApiConnection.ILogger {
+        AndyLogger() {}
+
+        @Override public void log(final String str) {
+            System.out.print(str);
+        }
+    }
+}
+
+// do clearing support
+// change from "New" to something else
+// more dn work, e.g. deltaNeutralValidation
+// add a "newAPI" signature
+// probably should not send F..A position updates to listeners, at least not to API; also probably not send FX positions; or maybe we should support that?; filter out models or include it
+// finish or remove strat panel
+// check all ps
+// must allow normal summary and ledger at the same time
+// you could create an enum for normal account events and pass segment as a separate field
+// check pacing violation
+// newticktype should break into price, size, and string?
+// give "already subscribed" message if appropriate
+
+// BUGS
+// When API sends multiple snapshot requests, TWS sends error "Snapshot exceeds 100/sec" even when it doesn't
+// When API requests SSF contracts, TWS sends both dividend protected and non-dividend protected contracts. They are indistinguishable except for having different conids.
+// Fundamentals financial summary works from TWS but not from API
+// When requesting fundamental data for IBM, the data is returned but also an error
+// The "Request option computation" method seems to have no effect; no data is ever returned
+// When an order is submitted with the "End time" field, it seems to be ignored; it is not submitted but also no error is returned to API.
+// Most error messages from TWS contain the class name where the error occurred which gets garbled to gibberish during obfuscation; the class names should be removed from the error message
+// If you exercise option from API after 4:30, TWS pops up a message; TWS should never popup a message due to an API request
+// TWS did not desubscribe option vol computation after API disconnect
+// Several error message are misleading or completely wrong, such as when upperRange field is < lowerRange
+// Submit a child stop with no stop price; you get no error, no rejection
+// When a child order is transmitted with a different contract than the parent but no hedge params it sort of works but not really, e.g. contract does not display at TWS, but order is working
+// Switch between frozen and real-time quotes is broken; e.g.: request frozen quotes, then realtime, then request option market data; you don't get bid/ask; request frozen, then an option; you don't get anything
+// TWS pops up mkt data warning message in response to api order
+
+// API/TWS Changes
+// we should add underConid for sec def request sent API to TWS so option chains can be requested properly
+// reqContractDetails needs primary exchange, currently only takes currency which is wrong; all requests taking Contract should be updated
+// reqMktDepth and reqContractDetails does not take primary exchange but it needs to; ideally we could also pass underConid in request
+// scanner results should return primary exchange
+// the check margin does not give nearly as much info as in TWS
+// need clear way to distinguish between order reject and warning
+
+// API Improvements
+// add logging support
+// we need to add dividendProt field to contract description
+// smart live orders should be getting primary exchange sent down
+
+// TWS changes
+// TWS sends acct update time after every value; not necessary
+// support stop price for trailing stop order (currently only for trailing stop-limit)
+// let TWS come with 127.0.0.1 enabled, same is IBG
+// we should default to auto-updating client zero with new trades and open orders
+
+// NOTES TO USERS
+// you can get all orders and trades by setting "master id" in the TWS API config
+// reqManagedAccts() is not needed because managed accounts are sent down on login
+// TickType.LAST_TIME comes for all top mkt data requests
+// all option ticker requests trigger option model calc and response
+// DEV: All Box layouts have max size same as pref size; but center border layout ignores this
+// DEV: Box layout grows items proportionally to the difference between max and pref sizes, and never more than max size
+
+//TWS sends error "Snapshot exceeds 100/sec" even when it doesn't; maybe try flush? or maybe send 100 then pause 1 second? this will take forever; i think the limit needs to be increased
+
+//req open orders can only be done by client 0 it seems; give a message
+//somehow group is defaulting to EqualQuantity if not set; seems wrong
+//i frequently see order canceled - reason: with no text
+//Missing or invalid NonGuaranteed value. error should be split into two messages
+//Rejected API order is downloaded as Inactive open order; rejected orders should never be sen
+//Submitting an initial stop price for trailing stop orders is supported only for trailing stop-limit orders; should be supported for plain trailing stop orders as well
+//EMsgReqOptCalcPrice probably doesn't work since mkt data code was re-enabled
+//barrier price for trail stop lmt orders why not for trail stop too?
+//All API orders default to "All" for F; that's not good
